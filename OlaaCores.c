99f36e7cb650a4d0414d0db274e8fe608b1ee2dc
lucas27716 <a27716@tomazpelayo.com>
2023-02-15 14:26:44 +0000

    exercicio

--- OlaaCores.c
+++ OlaaCores.c
@@ -1,3 +1,34 @@
 // Resolva neste ficheiro o seu exercício
+#include <stdio.h>
+#include <windows.h>
 
+int main()
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+	printf(" #### ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf("  #    ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("     ##\n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+	printf("#    #");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf("  #    ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("    #  #\n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+	printf("#    #");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf("  #    ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("   ######\n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+	printf(" #### ");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	printf("  #####");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	printf("  #      #");
+	return (0);
+}
 
